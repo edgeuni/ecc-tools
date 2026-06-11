2f7cb6fcd426f7611f1c2afe41fb6475c75dc195
ZhishengZeng <zhishengzeng@163.com>
2026-06-11 20:13:42 +0800

    add nSTA interface and toolkit skeleton

---
+++ src/operation/nSTA/source/toolkit/utility/Utility.hpp
@@ -0,0 +1,71 @@
+// ***************************************************************************************
+// Copyright (c) 2023-2025 Peng Cheng Laboratory
+// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of Sciences
+// Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
+//
+// iEDA is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+// http://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+// ***************************************************************************************
+#pragma once
+
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+namespace nsta {
+
+#define NSTAUTIL (nsta::Utility::getInst())
+
+class Utility
+{
+ public:
+  static void initInst();
+  static Utility& getInst();
+  static void destroyInst();
+
+  template <typename T, typename... Args>
+  std::string getString(T value, Args... args)
+  {
+    std::stringstream oss;
+    pushStream(oss, value, args...);
+    std::string string = oss.str();
+    oss.clear();
+    return string;
+  }
+
+  std::string formatSec(double seconds);
+  std::string formatByTwoDecimalPlaces(double value);
+
+ private:
+  static Utility* _util_instance;
+
+  Utility() = default;
+  Utility(const Utility& other) = delete;
+  Utility(Utility&& other) = delete;
+  ~Utility() = default;
+  Utility& operator=(const Utility& other) = delete;
+  Utility& operator=(Utility&& other) = delete;
+
+  template <typename Stream, typename T, typename... Args>
+  void pushStream(Stream& stream, T t, const Args&... args)
+  {
+    stream << t;
+    pushStream(stream, args...);
+  }
+
+  template <typename Stream, typename T>
+  void pushStream(Stream& stream, T t)
+  {
+    stream << t;
+  }
+};
+
+}  // namespace nsta
