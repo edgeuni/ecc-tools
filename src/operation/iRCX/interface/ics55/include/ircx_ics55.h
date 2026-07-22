2c019ab013e5f20583d58706a1d3c852b2a45510
ZhishengZeng <zhishengzeng@163.com>
2026-07-22 22:59:56 +0800

    Update ICS55 iRCX SDK artifacts

--- src/operation/iRCX/interface/ics55/include/ircx_ics55.h
+++ src/operation/iRCX/interface/ics55/include/ircx_ics55.h
@@ -16,19 +16,9 @@ extern "C" {
 #define IRCX_ICS55_API
 #endif
 
-// Returns non-zero on success and 0 on failure.
-IRCX_ICS55_API int ircx_ics55_init(const char* config_file);
-IRCX_ICS55_API int ircx_ics55_run(void);
-IRCX_ICS55_API int ircx_ics55_run_with_idb_design(void* idb_design);
-IRCX_ICS55_API int ircx_ics55_report(void);
-
-// Convenience API: init + run + report.
-IRCX_ICS55_API int ircx_ics55_extract(const char* config_file);
-IRCX_ICS55_API int ircx_ics55_extract_with_idb_design(const char* config_file, void* idb_design);
-
-// The returned pointer is owned by the library and remains valid until the next
-// call from the same thread.
-IRCX_ICS55_API const char* ircx_ics55_last_error(void);
+IRCX_ICS55_API void ircx_ics55_init(const char* config_file, void* idb_design);
+IRCX_ICS55_API void ircx_ics55_run(void);
+IRCX_ICS55_API void ircx_ics55_destroy(void);
 
 #ifdef __cplusplus
 }
