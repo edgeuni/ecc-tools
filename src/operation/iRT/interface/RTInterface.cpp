94212e62b3aeccd0d726eda314858bae34146a2f
shuyulun01 <3190100230@zju.edu.cn>
2026-07-26 11:00:34 +0800

    RT interface obs wrap perf update

--- src/operation/iRT/interface/RTInterface.cpp
+++ src/operation/iRT/interface/RTInterface.cpp
@@ -36,6 +36,8 @@
 #include "feature_manager.h"
 #include "idm.h"
 
+#include <unordered_map>
+
 namespace irt {
 
 // public
@@ -749,6 +751,15 @@ void RTInterface::wrapObstacleList()
   std::vector<idb::IdbInstance*>& idb_instance_list = idb_design->get_instance_list()->get_instance_list();
   std::vector<idb::IdbSpecialNet*>& idb_special_net_list = idb_design->get_special_net_list()->get_net_list();
   std::vector<idb::IdbPin*>& idb_io_pin_list = idb_design->get_io_pin_list()->get_pin_list();
+  std::unordered_map<idb::IdbNet*, bool> is_skipping_map;
+  is_skipping_map.reserve(idb_design->get_net_list()->get_num() + 1);
+  auto should_skip = [&](idb::IdbNet* idb_net) {
+    auto [iter, inserted] = is_skipping_map.try_emplace(idb_net);
+    if (inserted) {
+      iter->second = isSkipping(idb_net, false);
+    }
+    return iter->second;
+  };
   std::vector<idb::IdbLayer*> idb_routing_layer_list = dmInst->get_idb_lef_service()->get_layout()->get_layers()->get_routing_layers();
   std::vector<int32_t> active_routing_layer_id_list;
   if (!idb_routing_layer_list.empty()) {
@@ -788,7 +799,7 @@ void RTInterface::wrapObstacleList()
       }
       // instance pin without net
       for (idb::IdbPin* idb_pin : idb_instance->get_pin_list()->get_pin_list()) {
-        if (!isSkipping(idb_pin->get_net(), false)) {
+        if (!should_skip(idb_pin->get_net())) {
           continue;
         }
         for (idb::IdbLayerShape* port_box : idb_pin->get_port_box_list()) {
@@ -809,9 +820,10 @@ void RTInterface::wrapObstacleList()
       for (idb::IdbSpecialWire* idb_wire : idb_net->get_wire_list()->get_wire_list()) {
         for (idb::IdbSpecialWireSegment* idb_segment : idb_wire->get_segment_list()) {
           if (idb_segment->is_via()) {
-            total_routing_obstacle_num += idb_segment->get_via()->get_top_layer_shape().get_rect_list().size();
-            total_routing_obstacle_num += idb_segment->get_via()->get_bottom_layer_shape().get_rect_list().size();
-            total_cut_obstacle_num += idb_segment->get_via()->get_cut_layer_shape().get_rect_list().size();
+            idb::IdbViaMaster* via_master = idb_segment->get_via()->get_instance();
+            total_routing_obstacle_num += via_master->get_top_layer_shape()->get_rect_list().size();
+            total_routing_obstacle_num += via_master->get_bottom_layer_shape()->get_rect_list().size();
+            total_cut_obstacle_num += via_master->get_cut_layer_shape()->get_rect_list().size();
           } else {
             total_routing_obstacle_num += 1;
           }
@@ -820,7 +832,7 @@ void RTInterface::wrapObstacleList()
     }
     // io pin without net
     for (idb::IdbPin* idb_io_pin : idb_io_pin_list) {
-      if (!isSkipping(idb_io_pin->get_net(), false)) {
+      if (!should_skip(idb_io_pin->get_net())) {
         continue;
       }
       for (idb::IdbLayerShape* port_box : idb_io_pin->get_port_box_list()) {
@@ -868,7 +880,7 @@ void RTInterface::wrapObstacleList()
       }
       // instance pin without net
       for (idb::IdbPin* idb_pin : idb_instance->get_pin_list()->get_pin_list()) {
-        if (!isSkipping(idb_pin->get_net(), false)) {
+        if (!should_skip(idb_pin->get_net())) {
           continue;
         }
         for (idb::IdbLayerShape* port_box : idb_pin->get_port_box_list()) {
@@ -980,21 +992,24 @@ void RTInterface::wrapObstacleList()
       for (idb::IdbSpecialWire* idb_wire : idb_net->get_wire_list()->get_wire_list()) {
         for (idb::IdbSpecialWireSegment* idb_segment : idb_wire->get_segment_list()) {
           if (idb_segment->is_via()) {
-            for (idb::IdbLayerShape layer_shape : {idb_segment->get_via()->get_top_layer_shape(), idb_segment->get_via()->get_bottom_layer_shape()}) {
-              for (idb::IdbRect* rect : layer_shape.get_rect_list()) {
+            idb::IdbVia* idb_via = idb_segment->get_via();
+            idb::IdbViaMaster* via_master = idb_via->get_instance();
+            idb::IdbCoordinate<int32_t>* coordinate = idb_via->get_coordinate();
+            for (idb::IdbLayerShape* layer_shape : {via_master->get_top_layer_shape(), via_master->get_bottom_layer_shape()}) {
+              for (idb::IdbRect* rect : layer_shape->get_rect_list()) {
                 Obstacle obstacle;
-                obstacle.set_real_ll(rect->get_low_x(), rect->get_low_y());
-                obstacle.set_real_ur(rect->get_high_x(), rect->get_high_y());
-                obstacle.set_layer_idx(layer_shape.get_layer()->get_id());
+                obstacle.set_real_ll(rect->get_low_x() + coordinate->get_x(), rect->get_low_y() + coordinate->get_y());
+                obstacle.set_real_ur(rect->get_high_x() + coordinate->get_x(), rect->get_high_y() + coordinate->get_y());
+                obstacle.set_layer_idx(layer_shape->get_layer()->get_id());
                 routing_obstacle_list.push_back(std::move(obstacle));
               }
             }
-            idb::IdbLayerShape cut_layer_shape = idb_segment->get_via()->get_cut_layer_shape();
-            for (idb::IdbRect* rect : cut_layer_shape.get_rect_list()) {
+            idb::IdbLayerShape* cut_layer_shape = via_master->get_cut_layer_shape();
+            for (idb::IdbRect* rect : cut_layer_shape->get_rect_list()) {
               Obstacle obstacle;
-              obstacle.set_real_ll(rect->get_low_x(), rect->get_low_y());
-              obstacle.set_real_ur(rect->get_high_x(), rect->get_high_y());
-              obstacle.set_layer_idx(cut_layer_shape.get_layer()->get_id());
+              obstacle.set_real_ll(rect->get_low_x() + coordinate->get_x(), rect->get_low_y() + coordinate->get_y());
+              obstacle.set_real_ur(rect->get_high_x() + coordinate->get_x(), rect->get_high_y() + coordinate->get_y());
+              obstacle.set_layer_idx(cut_layer_shape->get_layer()->get_id());
               cut_obstacle_list.push_back(std::move(obstacle));
             }
           } else {
@@ -1011,7 +1026,7 @@ void RTInterface::wrapObstacleList()
     }
     // io pin without net
     for (idb::IdbPin* idb_io_pin : idb_io_pin_list) {
-      if (!isSkipping(idb_io_pin->get_net(), false)) {
+      if (!should_skip(idb_io_pin->get_net())) {
         continue;
       }
       for (idb::IdbLayerShape* port_box : idb_io_pin->get_port_box_list()) {
